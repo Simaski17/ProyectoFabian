bcbe9dc13475651c2c8bdf736f1f215c58416bbb
simaski <klomp60@gmail.com>
2016-11-08 01:16:55 -0300

    reestructurados fragments y agregado fragment promociones diseño xml

--- app/src/main/java/com/example/jimmyhernandez/tabletvendedor/HogarFragment.java
+++ app/src/main/java/com/example/jimmyhernandez/tabletvendedor/HogarFragment.java
@@ -1,57 +1,20 @@
 package com.example.jimmyhernandez.tabletvendedor;
 
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 public class HogarFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
 
-    public HogarFragment() {
-        // Required empty public constructor
-    }
-    public static HogarFragment newInstance(String param1, String param2) {
-        HogarFragment fragment = new HogarFragment();
-        return fragment;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hogar, container, false);
+
+
+        return v;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_hogar, container, false);
-        return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/example/jimmyhernandez/tabletvendedor/PlanesFragment.java
+++ app/src/main/java/com/example/jimmyhernandez/tabletvendedor/PlanesFragment.java
@@ -1,50 +1,20 @@
 package com.example.jimmyhernandez.tabletvendedor;
 
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
 
 public class PlanesFragment extends Fragment {
 
-    LinearLayout linearBuscardorPlanes, contentPlanes;
 
 
-    private OnFragmentInteractionListener mListener;
-
-    public PlanesFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_planes, container, false);
-        linearBuscardorPlanes = (LinearLayout) v.findViewById(R.id.linear_buscador_planes);
-        contentPlanes = (LinearLayout) v.findViewById(R.id.content_planes);
+
+
         return v;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
