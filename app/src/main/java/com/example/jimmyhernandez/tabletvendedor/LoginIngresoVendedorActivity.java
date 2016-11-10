2c7d431c9eaad606ae1b72328758219b80105b6f
Jimmy Hernandez <jimmyhernandezu@gmail.com>
2016-11-10 17:04:19 -0300

    cambios 10 de noviembre

--- app/src/main/java/com/example/jimmyhernandez/tabletvendedor/LoginIngresoVendedorActivity.java
+++ app/src/main/java/com/example/jimmyhernandez/tabletvendedor/LoginIngresoVendedorActivity.java
@@ -1,10 +1,14 @@
 package com.example.jimmyhernandez.tabletvendedor;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +23,8 @@ public class LoginIngresoVendedorActivity extends AppCompatActivity {
     EditText etRutVendedor;
     @BindView(R.id.btEntrarVendedor)
     Button btEntrarVendedor;
+    @BindView(R.id.activity_login_ingreso_vendedor)
+    LinearLayout activityLoginIngresoVendedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +33,19 @@ public class LoginIngresoVendedorActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.btEntrarVendedor)
-    public void onClick() {
-        Intent miIntent = new Intent(LoginIngresoVendedorActivity.this,MainActivity.class);
-        LoginIngresoVendedorActivity.this.startActivity(miIntent);
-        LoginIngresoVendedorActivity.this.finish();
+    @OnClick({R.id.btEntrarVendedor, R.id.activity_login_ingreso_vendedor})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btEntrarVendedor:
+                Intent miIntent = new Intent(LoginIngresoVendedorActivity.this, IngresoVendedorClienteActivity.class);
+                LoginIngresoVendedorActivity.this.startActivity(miIntent);
+                LoginIngresoVendedorActivity.this.finish();
+                break;
+            case R.id.activity_login_ingreso_vendedor:
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(etNombreVendedor.getWindowToken(), 0);
+                break;
+        }
     }
+
 }
